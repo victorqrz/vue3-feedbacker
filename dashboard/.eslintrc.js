3098af9dbb14a84c47edbe77ef547d591b827840
Victor Queiroz <victoorqrz@gmail.com>
2022-08-01 19:13:50 -0300

    creating Contact component

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -1,33 +1,37 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       rules: {
         'vue/multi-word-component-names': [
           'error',
           {
-            ignores: ['index'],
-          },
-        ],
+            ignores: ['index']
+          }
+        ]
       },
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 }
--- dashboard/src/views/Home/HomeIndex.vue
+++ dashboard/src/views/Home/HomeIndex.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+  <custom-header @create-account="handleLogin" @login="handleLogin" />
+  <contact />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker &copy; 2022</p>
+  </footer>
 </template>
 
 <script>
-export default {}
-</script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import Contact from './HomeContact.vue'
+import CustomHeader from './CustomHeader.vue'
+export default {
+  components: { Contact, CustomHeader },
+  setup() {
+    const router = useRouter()
 
-<style></style>
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
